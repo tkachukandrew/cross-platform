48a08d0f786c892cfac3d8ae8295349b541dcfe1
Andrew Tkachuk <andrewtkachuk@yahoo.com>
2018-03-18 15:04:15 +0200

    Add macOS project

---
+++ TimePlannerMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimePlannerMac
+//
+//  Created by Andrew Tkachuk on 18.03.18.
+//  Copyright © 2018 Andrew Tkachuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimePlannerMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimePlannerMac
+//
+//  Created by Andrew Tkachuk on 18.03.18.
+//  Copyright © 2018 Andrew Tkachuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
